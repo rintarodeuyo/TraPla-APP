6034594ab4de097565c57d21d74d56c11516a65d
Uu Rintaro <>
2024-12-28 16:17:47 +0900

    プロジェクト追加

---
+++ telaPla/telaPla/telaPlaApp.swift
@@ -0,0 +1,17 @@
+//
+//  telaPlaApp.swift
+//  telaPla
+//
+//  Created by Uu Rintaro on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct telaPlaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
